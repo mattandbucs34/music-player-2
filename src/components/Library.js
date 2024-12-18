1419227ee5a57e60c9f6b74e9151b6a5c1c36e3e
Matt Johnson <mattandbucs@gmail.com>
2024-12-18 17:38:33 -0600

    begin upgrade process

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router';
 import albumData from './../data/albums';
 
 class Library extends Component {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-        , document.getElementById('root'));
-registerServiceWorker();
+  </StrictMode>
+);
